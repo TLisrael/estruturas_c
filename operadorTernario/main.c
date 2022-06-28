dc084be11abd31e3117f2cff1a3ed7eb4a966f74
Thaina Israel <thainaluizaisrael@gmail.com>
2022-06-28 16:13:17 -0300

    operador ternário

---
+++ operadorTernario/main.c
@@ -0,0 +1,17 @@
+// if == ?; else ==  : 
+// Não é muito utilizado. 
+
+#include <stdio.h>
+
+int main (){
+
+
+  puts ("Teste Ternario");
+  int x, y;
+  x = 7;
+
+  (x > 0) ? 22 : -15;
+  printf ("y=%i" , y);
+
+  return 0;
+}
